0c00041cd5c08515c5daf93255bbd679ec8eb202
peach <peach@dawn.com>
2025-04-16 22:16:03 +0800

    init:增加日志

--- dawn-chaos/src/main/java/com/dawn/chaos/DawnChaosApplication.java
+++ dawn-chaos/src/main/java/com/dawn/chaos/DawnChaosApplication.java
@@ -1,13 +1,16 @@
 package com.dawn.chaos;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class DawnChaosApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DawnChaosApplication.class, args);
+        log.info("============Dawn chaos application started=========");
     }
 
 }
