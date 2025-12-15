0abae12d246219ab92d08a963a9671c01485eb47
peach <peach@dawn.com>
2025-12-15 21:45:53 +0800

    init:增加spring-websocket

---
+++ dawn-websocket/src/main/java/com/dawn/websocket/DawnWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.dawn.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawnWebsocketApplication.class, args);
+    }
+
+}
---
+++ dawn-websocket/src/test/java/com/dawn/websocket/DawnWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawn.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnWebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
